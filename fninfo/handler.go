89d8f50c2feb4799c29dab66359502687074e630
Stefan Prodan <stefan.prodan@gmail.com>
2018-05-24 23:34:47 +0300

    Read openfaas-fn kubernetes deployments and env - list pods, deployments, services - print env - print request

---
+++ fninfo/handler.go
@@ -0,0 +1,90 @@
+package function
+
+import (
+	"encoding/json"
+	"log"
+	"os"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+)
+
+// Handle a serverless request
+func Handle(req []byte) string {
+
+	cfg, err := rest.InClusterConfig()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	kubeClient, err := kubernetes.NewForConfig(cfg)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ns := "openfaas-fn"
+	if namespace, exists := os.LookupEnv("function_namespace"); exists {
+		ns = namespace
+	}
+
+	pods, err := kubeClient.CoreV1().Pods(ns).List(metav1.ListOptions{})
+	if err != nil {
+		log.Fatalln("failed to read pods:", err)
+	}
+
+	var podList []string
+	for _, pod := range pods.Items {
+		podList = append(podList, pod.GetName())
+	}
+
+	svc, err := kubeClient.CoreV1().Services(ns).List(metav1.ListOptions{})
+	if err != nil {
+		log.Fatalln("failed to read services:", err)
+	}
+
+	var svcList []string
+	for _, s := range svc.Items {
+		svcList = append(svcList, s.GetName())
+	}
+
+	sec, err := kubeClient.AppsV1beta1().Deployments(ns).List(metav1.ListOptions{})
+	if err != nil {
+		log.Fatalln("failed to read deployments:", err)
+	}
+
+	var secList []string
+	for _, s := range sec.Items {
+		secList = append(secList, s.GetName())
+	}
+
+	host, err := os.Hostname()
+	if err != nil {
+		log.Fatalln("failed to read hostname:", err)
+	}
+
+	r := Response{
+		host,
+		podList,
+		svcList,
+		secList,
+		os.Environ(),
+		string(req),
+	}
+
+	rb, err := json.Marshal(r)
+	if err != nil {
+		log.Fatalln("failed to serialize respose:", err)
+	}
+
+	return string(rb)
+}
+
+type Response struct {
+	Hostname    string
+	Pods        []string
+	Services    []string
+	Deployments []string
+	Environment []string
+	Request     string
+}
